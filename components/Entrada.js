8cfc83c4966fc94f55c966c53fc4df79691c1976
jose ignacio <josignaciot@gmail.com>
2021-12-29 17:35:36 -0300

    Showing the rest of the information

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+import { formatearFecha } from '../helpers/index';
+
 const Entrada = ({ entrada }) => {
-  const { titulo, resumen, imagen, published_at } = entrada;
+  const { titulo, resumen, imagen, published_at, id } = entrada;
 
   return (
     <article>
       <h1>{titulo}</h1>
+      <p>{formatearFecha(published_at)}</p>
+      <p>{resumen}</p>
+      <Link href={`/blog/${id}`}>Leer Entrada</Link>
     </article>
   );
 };
