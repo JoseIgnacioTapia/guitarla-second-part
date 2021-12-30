0a953fc0ca2b566ea4a36ae9c1f35b382297eee3
jose ignacio <josignaciot@gmail.com>
2021-12-30 20:43:54 -0300

    Querying the API for Blog entry information

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -8,7 +8,22 @@ const EntradaBlog = ({ entrada }) => {
   );
 };
 
-export async function getServerSideProps({ query: { id } }) {
+export async function getStaticPaths() {
+  const url = 'http://localhost:1337/blogs';
+  const respuesta = await fetch(url);
+  const entradas = await respuesta.json();
+
+  const paths = entradas.map(entrada => ({
+    params: { id: entrada.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { id } }) {
   const url = `http://localhost:1337/blogs/${id}`;
   const respuesta = await fetch(url);
   const entrada = await respuesta.json();
@@ -20,5 +35,18 @@ export async function getServerSideProps({ query: { id } }) {
     },
   };
 }
+
+// export async function getServerSideProps({ query: { id } }) {
+//   const url = `http://localhost:1337/blogs/${id}`;
+//   const respuesta = await fetch(url);
+//   const entrada = await respuesta.json();
+//   console.log(entrada);
+
+//   return {
+//     props: {
+//       entrada: entrada,
+//     },
+//   };
+// }
 
 export default EntradaBlog;
