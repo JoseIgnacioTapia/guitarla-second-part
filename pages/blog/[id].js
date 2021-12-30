5df360faae9433961086adcdaee960ade0e29083
jose ignacio <josignaciot@gmail.com>
2021-12-30 18:28:02 -0300

    Dynamic Routing in Next.js

---
+++ pages/blog/[id].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+
+const EntradaBlog = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>Desde EntradaBlog</h1>
+    </div>
+  );
+};
+
+export default EntradaBlog;
