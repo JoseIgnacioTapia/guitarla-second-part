d011661ed6cd2130e0384797f45ae623b5caa343
jose ignacio <josignaciot@gmail.com>
2022-01-03 16:57:35 -0300

    Consult the API to obtain the guitars

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,11 +1,29 @@
 import Layout from '../components/Layout';
 
-const Tienda = () => {
+const Tienda = ({ guitarras }) => {
+  console.log(guitarras);
+
   return (
     <Layout pagina="Tienda Virtual">
-      <h1>Desde Tienda</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colleción</h1>
+      </main>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras`;
+  const respuesta = await fetch(url);
+  const guitarras = await respuesta.json();
+
+  console.log(guitarras);
+
+  return {
+    props: {
+      guitarras,
+    },
+  };
+}
 
 export default Tienda;
